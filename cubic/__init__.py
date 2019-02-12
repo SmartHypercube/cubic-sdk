9cf6e47f1fb1e0ac8c65fb1de287f0f3c080f47b
Hypercube <hypercube@0x01.me>
2019-02-12 14:33:35 +0800

    v1.0

--- cubic/__init__.py
+++ cubic/__init__.py
@@ -1,108 +1,87 @@
 from base64 import b64decode, b64encode
+from collections import namedtuple
 from hashlib import sha3_256
 import requests
 
+__all__ = ['HASH', 'HASH_LEN', 'Cubic', 'Item']
 
 HASH = sha3_256
 HASH_LEN = len(HASH().hexdigest())
 
-
-def hash(data):
-    return HASH(data).hexdigest()
+Item = namedtuple('Item', ('path', 'meta', 'blocks'))
 
 
 def b64(data):
     return b64encode(data).decode('ascii')
 
 
-class Node:
-    def __init__(self, path, is_dir=False, meta=b'', children=(), size=0, blocks=()):
-        """
-        :param path: bytes
-        :param is_dir: bool
-        :param meta: bytes
-        :param children: list of Nodes
-        :param size: int
-        :param blocks: list of hashes
-        """
-        self.path = path
-        self.is_dir = bool(is_dir)
-        self.meta = meta
-        self.children = list(children)
-        self.size = int(size)
-        self.blocks = list(blocks)
+def async_map(func, iterable):
+    from asyncio import get_event_loop
+    from concurrent.futures import ThreadPoolExecutor
 
-    @classmethod
-    def from_json(cls, data):
-        if not data['is_dir']:
-            return cls(b64decode(data['path']), False, b64decode(data['meta']), size=data['size'],
-                       blocks=data['blocks'])
-        return cls(b64decode(data['path']), True, b64decode(data['meta']),
-                   children=map(cls.from_json, data['children']))
+    loop = get_event_loop()
+    executor = ThreadPoolExecutor(20)
 
-    def __str__(self):
-        return self.path.decode('utf8', errors='ignore')
-
-    def __repr__(self):
-        return repr(self.path)
+    async def coroutine():
+        futures = [loop.run_in_executor(executor, func, i) for i in iterable]
+        return [await f for f in futures]
+    return loop.run_until_complete(coroutine())
 
 
-class CubicServer:
-    class Error(Exception):
-        pass
+class Cubic:
+    _hfs_get = 'https://hfs-1251965935.cos.ap-shanghai.myqcloud.com/'
+    _hfs_api = 'https://service-1p2s8p0h-1251965935.ap-shanghai.apigateway.myqcloud.com/release/'
+    _cubic_api = 'https://service-qkomvv9d-1251965935.ap-shanghai.apigateway.myqcloud.com/release/'
 
-    def __init__(self, url):
-        self.url = url
-        self.session = requests.Session()
+    def __init__(self, user, token):
+        self._session = requests.Session()
+        self._cubic_api += f'{user}/{token}'
 
-    def __call__(self, api, **kwargs):
-        response = self.session.post(self.url, json=dict(api=api, **kwargs)).json()
-        if response['error'] is None:
-            return response['result']
-        raise self.Error(response['error'])
+    def head_block(self, hash):
+        """Returns True if server has the block. False if not. None if request failed."""
+        res = self._session.head(self._hfs_get + hash)
+        return res.ok if res.ok or res.status_code == 404 else None
 
-    def check_hash(self, hash_list):
-        """
-        :param hash_list: list of hashes
-        :return: hashes which can be skipped
-        """
-        return self('check_hash', l=list(hash_list))
+    def get_block(self, hash):
+        """Returns block in bytes. None if request failed."""
+        res = self._session.get(self._hfs_get + hash)
+        return res.content if res.ok else None
 
-    def put_block(self, blocks):
-        """
-        :param blocks: dict of (hash, bytes)
-        :return: None
-        """
-        return self('put_block', l=[{'hash': h, 'data': b64(blocks[h])} for h in blocks])
+    def post_block(self, block):
+        """Uploads block in bytes. Returns the hash. None if request failed."""
+        res = self._session.post(self._hfs_api, b64encode(block))
+        return res.text if res.ok else None
 
-    def update_tree(self, *, add=(), remove=()):
-        """
-        :param add: list of Nodes
-        :param remove: list of Nodes
-        :return: None
-        """
-        return self(
-            'update_tree',
-            add=[{'path': b64(n.path), 'is_dir': n.is_dir, 'meta': b64(n.meta), 'blocks': n.blocks} for n in add],
-            remove=[{'path': b64(n.path)} for n in remove],
-        )
+    def get_tree(self):
+        """Returns Items in the tree. None if request failed."""
+        res = self._session.get(self._cubic_api)
+        if not res.ok:
+            return
+        items = []
+        for line in res.iter_lines():
+            path, meta, blocks = line.split(b':')
+            items.append(Item(b64decode(path), b64decode(meta), blocks.decode('ascii').split(',')))
+        return items
 
-    def get_tree(self, base):
-        """
-        :param base: Node
-        :return: Node
-        """
-        return Node.from_json(self('get_tree', base=b64(base.path)))
+    def post_tree(self, put_items=(), delete_paths=()):
+        """Updates the tree. Returns the new version ID. None if request failed."""
+        res = self._session.post(self._cubic_api, json={
+            'put_list': [{
+                'path': b64(i.path),
+                'meta': b64(i.meta),
+                'blocks': list(i.blocks)} for i in put_items],
+            'delete_list': [{'path': b64(p)} for p in delete_paths],
+        })
+        return res.text if res.ok else None
 
-    def get_block(self, hash_list):
-        """
-        :param hash_list: list of hashes
-        :return: dict of (hash, bytes)
-        """
-        return {h: b64decode(d) for h, d in self('get_block', l=list(hash_list)).items()}
+    def bulk_head_block(self, hashes):
+        """Calls head_block in bulk."""
+        return async_map(self.head_block, hashes)
 
-    def reset(self):
-        """
-        :return: None
-        """
-        return self('reset')
+    def bulk_get_block(self, hashes):
+        """Calls get_block in bulk."""
+        return async_map(self.get_block, hashes)
+
+    def bulk_post_block(self, blocks):
+        """Calls post_block in bulk."""
+        return async_map(self.post_block, blocks)
